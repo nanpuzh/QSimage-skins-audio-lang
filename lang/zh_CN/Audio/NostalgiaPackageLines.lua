5f4ba84242a8e50d629bb42e60f03b5b5d8a8610
dragon100a <dragon100a@gmail.com>
2013-09-10 22:06:57 +0800

    origin "lang" folder

---
+++ lang/zh_CN/Audio/NostalgiaPackageLines.lua
@@ -0,0 +1,76 @@
+-- lines for Nostalgia Package
+
+return {
+	-- 旧凌统
+	["cv:nos_lingtong"] = "官方",
+	["$nosxuanfeng1"] = "伤敌于千里之外！",
+	["$nosxuanfeng2"] = "索命于须臾之间！",
+	["~nos_lingtong"] = "大丈夫……不惧死亡……",
+
+	-- 旧关兴张苞
+	["cv:nos_guanxingzhangbao"] = "官方",
+	["$nosfuhun1"] = "以父之名，为国而战！",
+	["$nosfuhun2"] = "父亲的遗志，由我继承！",
+	["$~nos_guanxingzhangbao"] = "父亲……我来了……",
+
+	-- 旧韩当
+	["cv:nos_handang"] = "官方",
+	["$nosgongqi1"] = "吃我一箭！",
+	["$nosgongqi2"] = "鼠辈，哪里走！",
+	["$nosjiefan1"] = "退后，这里交给我！",
+	["$nosjiefan2"] = "休要趁人之危！",
+
+	-- 旧马岱
+	["cv:nos_madai"] = "风叹息",
+	["$nosqianxi1"] = "（拔剑声）",
+	["$nosqianxi2"] = "我敢杀你！",                   -- 成功
+	["$nosqianxi3"] = "竟然有防备！只能力战搏杀之！ ", -- 潜袭失败
+	["~nos_madai"] = "未能完成丞相遗命……辱没了我马家的威名呀……",
+
+	-- 旧王异
+	["cv:nos_wangyi"] = "官方",
+	["$noszhenlie1"] = "我，绝不屈服！", -- 回合外发动
+	["$noszhenlie2"] = "看看我的觉悟吧！", -- 回合内发动
+	["$nosmiji1"] = "奇谋，只在绝境中诞生！",
+	["$nosmiji2"] = "我将尽我所能！", -- 分给自己
+	["$nosmiji3"] = "我将尽我所能！", -- 分给别人
+	["$nosmiji4"] = "我将尽我所能！", -- 分给马超
+	["~nos_wangyi"] = "忠义已尽……死又何妨……",
+
+	["$nosenyuan1"] = "得人恩果千年记。",
+	["$nosenyuan2"] = "滴水之恩，涌泉以报。",
+	["$nosenyuan3"] = "谁敢得罪我！",
+	["$nosenyuan4"] = "睚眦之怨，无不报复。",
+	["$nosxuanhuo1"] = "重用许靖，以眩远近。",
+	["$nosxuanhuo2"] = "给你的，十倍奉还给我！",
+	["~nos_fazheng"] = "蜀翼既折…蜀汉哀矣……",
+
+	["$noswuyan1"] = "唉，一切尽在不言中。",
+	["$noswuyan2"] = "嘘，言多必失啊。",
+	["$nosjujian1"] = "我看好你。",
+	["$nosjujian2"] = "将军岂愿抓牌呼？",
+	["~nos_xushu"] = "娘…孩儿不孝…向您…请罪…",
+
+	["$nosshangshi1"] = "无情者伤人，有情者自伤。",
+	["$nosshangshi2"] = "自损八百，可伤敌一千！",
+	["~nos_zhangchunhua"] = "怎能如此对我……",
+
+	["$nosrende1"] = "以德服人。",
+	["$nosrende2"] = "惟贤惟德，能服于人。",
+	["$nosjizhi1"] = "哼哼！",
+	["$nosjizhi2"] = "哼！",
+	["$noslijian1"] = "（暗笑声）",
+	["$noslijian2"] = "夫君，你要替妾身做主啊。",
+
+    ["$nosjushou1"] = "我先休息一会儿！",
+	["$nosjushou2"] = "尽管来吧！",
+
+	["$nosleiji1"] = "雷公助我！",
+	["$nosleiji2"] = "以我之真气，合天地之造化。",
+
+	["$nosguhuo1"] = "你信吗？",
+	["$nosguhuo2"] = "猜猜看哪？",
+
+    ["$nosbuqu1"] = "我绝不会倒下！",
+	["$nosbuqu2"] = "还不够！",
+}
